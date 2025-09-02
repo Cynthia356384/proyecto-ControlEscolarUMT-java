9daf0135e3348fdf4c45c22380aa1ba9b507c4ce
Cynthia356384 <cynthiaalessandraaguilaruicab@gmail.com>
2025-09-02 21:43:46 +0000

    Mi primer commit

---
+++ src/org/ejercicio/ControlEscolar/Main.java
@@ -0,0 +1,8 @@
+package org.ejercicio.ControlEscolar;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona p = new Persona("Ana", 21);
+        System.out.println("Hola, " + p + "!");
+    }
+}
